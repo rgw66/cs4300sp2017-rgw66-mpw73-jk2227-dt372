0db8cc77ab10cbf73896645a8938ca37b83155b4
Ji Hun Kim <jk2227@cornell.edu>
2017-04-18 08:54:07 -0400

    progress

--- project_template/test.py
+++ project_template/test.py
@@ -3,7 +3,28 @@ import os
 import Levenshtein
 import json
 import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
+
+def get_hotel_tuples(hotel_information, ta_reviews):
+	hotel_to_key_index = {} 
+	index_to_hotel = {}
+	reviews = [] 
+	i = 0 
+	index_to_hotel = {} 
+	for hotel_name in hotel_information:
+		hotel_to_key_index[hotel_name] = i 
+		i += 1
+	i = 0 
+	for ta_review in ta_reviews: 
+		reviews.append(ta_review['review'])
+		index_to_hotel[i] = hotel_to_key_index[ta_review['hotel_name']]
+	print "lol"
+	print len(reviews)
+	return (hotel_to_key_index,
+		index_to_hotel, 
+		reviews)
 
 def read_file(n):
 	path = Docs.objects.get(id = n).address;
@@ -13,6 +34,32 @@ def read_file(n):
 
 def _edit(query, msg):
     return Levenshtein.distance(query.lower(), msg.lower())
+
+def search_svd(query, vectorizer, words_compressed, docs_compressed, top_k = 10):
+    vec = vectorizer.transform([query])
+    results = np.dot(vec * words_compressed, docs_compressed)
+    indices = np.squeeze(np.asarray(np.argsort(results)[:,::-1][:,:top_k]))
+    scores = np.squeeze(np.asarray(np.sort(results)[:,::-1][:,:top_k]))
+    listings = np.zeros(indices.shape)
+    for i in range(indices.shape[0]):
+        for j in range(indices.shape[1]):
+            listings[i,j] = index_to_hotel[indices[i,j]]
+    return (listings, indices, scores)
+
+def get_hotel_results(query, ta_reviews, hotel_information, vectorizer, words_compressed, docs_compressed):
+	hotels, indices, scores = search_svd(query, vectorizer, words_compressed, docs_compressed)
+	hotel_keys = hotel_information.keys()
+	for (l,ind, score) in zip(hotels, indices, scores):
+		hotel_id = int(l)
+        review_info = ta_reviews[ind] 
+        hotel_info = hotel_information[hotel_keys[hotel_id]]
+        print("Hotel ID: %i" % hotel_id)
+        print ("Hotel name: " + hotel_keys[hotel_id])
+        print ("Review Title: %s" % review_info['title'])
+        print("Review: \n" + review_info['review'])
+        print ("Hotel URL: " + hotel_info[0])
+        print("Score (Similarity): " + str(score))
+        print("**********************")
 
 def find_similar(q):
 	transcripts = read_file(1)
--- project_template/views.py
+++ project_template/views.py
@@ -4,9 +4,37 @@ from django.http import HttpResponse
 from .models import Docs
 from django.template import loader
 from .form import QueryForm
-from .test import find_similar
+from .test import get_hotel_results, get_hotel_tuples
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import json
+import cPickle as pickle
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+
+with open('data/tripadvisor_reviews.pickle','rb') as f:
+  ta_reviews_data = pickle.load(f)
+
+with open('data/tripadvisor_hotel_info.pickle','rb') as f:
+  hotel_information = pickle.load(f)
+
+(hotel_to_key_index, index_to_hotel, ta_reviews) = get_hotel_tuples(hotel_information, ta_reviews_data)
+
+ta_vectorizer = TfidfVectorizer(stop_words='english', max_df = 0.7)
+ta_vectorizer.fit_transform(ta_reviews)
+
+with open('data/10_by_docs_svd_ta.pickle','rb') as f:
+  docs_compressed_ta = pickle.load(f) 
+
+with open('data/words_by_10_svd_ta.pickle','rb') as f:
+  words_compressed_ta = pickle.load(f) 
+
+with open('data/airbnb_reviews.pickle','rb') as f:
+  airbnb_reviews = pickle.load(f)
+
+with open('data/airbnb_listings.pickle','rb') as f:
+  listings_information = pickle.load(f)
+
+
 
 hotel_review = {'review': 'This is a wonderful hotel. Well proportioned room. I loved the decor. The restaurant and staff were great. Extra big shower caps was only the tell tale sign that they take care of all the details. Really friendly and helpful staff.', 'review_stars': '5 of 5 bubbles', 'hotel_name':  'Gramercy Park Hotel' , 'title': 'Beautiful decor'}
 
@@ -18,12 +46,18 @@ airbnb_list = [airbnb_review for _ in range(10)]
 
 # Create your views here.
 def index(request):
+    print len(ta_reviews)
+    print docs_compressed_ta.shape
+    print words_compressed_ta.shape
     hotel_output = []
     airbnb_output = []
     words=json.load(open("jsons/words.json"))
     if request.GET.get('search'):
-        hotel_output = hotel_list
-        airbnb_output = airbnb_list
+      query = request.GET.get('search')
+      get_hotel_results(query, ta_reviews, hotel_information, ta_vectorizer, words_compressed_ta, docs_compressed_ta)
+      
+        #hotel_output = hotel_list
+        #airbnb_output = airbnb_list
         # search = request.GET.get('search')
         # output_list = find_similar(search)
         # paginator = Paginator(output_list, 10)
