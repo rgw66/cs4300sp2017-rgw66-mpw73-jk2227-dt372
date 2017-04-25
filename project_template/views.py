e124c21eadaca49ce817b26b81d770c69202bce1
rgw66 <rgw66@cornell.edu>
2017-04-24 23:42:56 -0400

    Initial hotel listings. Minimul front end changes

--- project_template/views.py
+++ project_template/views.py
@@ -4,66 +4,31 @@ from django.http import HttpResponse
 from .models import Docs
 from django.template import loader
 from .form import QueryForm
-from .test import get_hotel_results, get_hotel_tuples, get_airbnb_tuples, get_airbnb_results
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from main import get_airbnb_results, get_hotel_results
 import json
-import cPickle as pickle
-from scipy.sparse.linalg import svds
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+# from .test import get_hotel_results, get_hotel_tuples, get_airbnb_tuples, get_airbnb_results
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-with open('data/tripadvisor_reviews.pickle','rb') as f:
-  ta_reviews_data = pickle.load(f)
-
-with open('data/tripadvisor_hotel_info.pickle','rb') as f:
-  hotel_information = pickle.load(f)
-
-(hotel_to_key_index, index_to_hotel, ta_reviews) = get_hotel_tuples(hotel_information, ta_reviews_data)
-
-ta_vectorizer = TfidfVectorizer(stop_words='english', max_df = 0.7)
-ta_tfidf = ta_vectorizer.fit_transform(ta_reviews)
-
-#words_compressed_ta, _, docs_compressed_ta = svds(ta_tfidf, k=10)
-docs_compressed_ta, _, words_compressed_ta = svds(ta_tfidf, k=10)
-docs_compressed_ta = docs_compressed_ta.T
-words_compressed_ta = words_compressed_ta.T
-
-#with open('data/10_by_docs_svd_ta.pickle','rb') as f:
-#  words_compressed_ta = pickle.load(f) 
-
-#with open('data/words_by_10_svd_ta.pickle','rb') as f:
-#  docs_compressed_ta = pickle.load(f) 
-
-with open('data/airbnb_reviews.pickle','rb') as f:
- airbnb_reviews_data = pickle.load(f)
-
-with open('data/airbnb_listings.pickle','rb') as f:
- listings_information = pickle.load(f)
-
-(index_to_listing, airbnb_reviews) = get_airbnb_tuples(airbnb_reviews_data)
-
-airbnb_vectorizer = TfidfVectorizer(stop_words='english', max_df = 0.7)
-airbnb_tfidf = airbnb_vectorizer.fit_transform(airbnb_reviews)
-
-docs_compressed_airbnb, _, words_compressed_airbnb = svds(airbnb_tfidf, k=10)
-docs_compressed_airbnb = docs_compressed_airbnb.T
-words_compressed_airbnb = words_compressed_airbnb.T
 
 # Create your views here.
 def index(request):
     words=json.load(open("jsons/words.json"))
     airbnb_output = []
     hotel_output = []
+    best_result = {}
     query = ''
+
     if request.GET.get('search'):
-      query = request.GET.get('search')
+        query = request.GET.get('search')
+        airbnb_output = get_airbnb_results(query)
+        hotel_output = get_hotel_results(query)
 
-      hotel_output = get_hotel_results(query, ta_reviews_data, hotel_information, index_to_hotel, ta_vectorizer, words_compressed_ta, docs_compressed_ta)
-      airbnb_output = get_airbnb_results(query, airbnb_reviews, listings_information, index_to_listing, airbnb_vectorizer, words_compressed_airbnb, docs_compressed_airbnb)
+        if airbnb_output[0]['score'] > hotel_output[0]['score']:
+            best_result = airbnb_output[0]
+        else:
+            best_result = hotel_output[0]
 
-      print hotel_output[0]
-      print airbnb_output[0]
-        #hotel_output = hotel_list
-        #airbnb_output = airbnb_list
+        ## KEEP THIS. This will be helpful if we want to do pages
         # search = request.GET.get('search')
         # output_list = find_similar(search)
         # paginator = Paginator(output_list, 10)
@@ -79,6 +44,7 @@ def index(request):
                           {'airbnb_output': airbnb_output,
                            'search': query,
                            'hotel_output': hotel_output,
+                           'best_result': best_result,
                            'magic_url': request.get_full_path(),
                            'words': words,
                            })
